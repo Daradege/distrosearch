98c5d30f5fc4fc6c5f5064ec1f01188b074d797f
Ali Safamanesh <darg.q.a.a@gmail.com>
2025-07-12 15:18:00 +0330

    ♻️ refactor(core): Used python and github actions to generate pages

---
+++ generate.py
@@ -0,0 +1,33 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+import json
+
+OUTPUT_DIR = "output"
+env = Environment(loader=FileSystemLoader('templates'))
+
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+with open("database.json", "r") as f:
+    distros = json.load(f)
+    f.close()
+
+with open("desktops.json", "r") as d:
+    desktop = json.load(d)
+    d.close()
+
+
+all_bases = distros["bases"]
+all_distros = []
+
+with open(os.path.join(OUTPUT_DIR)+"/desktops.html", "w", encoding="utf-8") as f:
+    template = env.get_template('desktops.html')
+    f.write(template.render(desktops=desktop))
+
+with open(os.path.join(OUTPUT_DIR)+"/distros.html", "w", encoding="utf-8") as f:
+    template = env.get_template('distros.html')
+    f.write(template.render(bases=all_bases))
+
+os.system("cp -r assets " + OUTPUT_DIR)
+os.system("cp -r src " + OUTPUT_DIR)
+
+print("HTML generation done.")
